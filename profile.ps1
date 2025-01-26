d4e8735d35b09248925723361034dbdad4716cfc
sinkhwal07 <129762050+sinkhwal07@users.noreply.github.com>
2025-01-26 16:04:21 +0545

    Add files via upload

---
+++ profile.ps1
@@ -0,0 +1,4 @@
+# Automatically activate the virtual environment in the project directory
+if (Test-Path "C:\Users\nabin\OneDrive\Desktop\danfe\danfe\env\Scripts\Activate.ps1") {
+    . "C:\Users\nabin\OneDrive\Desktop\danfe\danfe\env\Scripts\Activate.ps1"
+}
